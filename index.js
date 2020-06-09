4ef2ee0fc610a8367afa226835fcee1b37f2fa77
Ashton <ashtoturn@gmail.com>
2020-06-09 15:54:22 -0500

    half way done

--- index.js
+++ index.js
@@ -4,7 +4,10 @@
 /* Create variables for principal, interest rate, and years. Assign them the values 200000, 0.05, and 30 respectively. Create another value called name and give it the value of your own name.
 */
 
-
+let prince = 200000;
+let int = 0.05;
+let years = 30;
+const name = "Ashton";
 
 
 
@@ -14,7 +17,9 @@
 (1) Create a variable called `monthlyInterestRate` and give it the value of interest rate divided by 12. 
 (2) Create another variable called `periods` and give it the value of years*12.
 */
-
+let monthlyInterestRate = int / 12;
+let periods = years * 12;
+let N = periods
 
 
 
@@ -35,19 +40,23 @@ Hint #2: you'll need to use the `math` object for parts of this calculation!
 
 When your math is correct, monthlyRate will equal 1073.64
 */
+let n1 = Math.pow((1 + monthlyInterestRate),N);
+let n2 = n1 * monthlyInterestRate;
+// let numerator = n1 * prince;
+let denominator = n1 - 1;
+let monthlyRate = prince * (n2/denominator);
 
-
-
+// console.log(monthlyRate);
 
 // 🏡 Task 3: Function
 /* Create a function called `mortgageCalculator` that combines all of the steps from task 1 and 2 and returns a sentence "{Name}, your monthly rate is ${monthlyRate}"
 
 If your name is `Oscar` mortgageCalculator() should return "Oscar, your monthly rate is 1073.64"
 */
-
-
-
-
+function mortgageCalculator(){
+    return `${name}, your monthly rate is ${monthlyRate.toFixed(2)}`;
+}
+console.log(mortgageCalculator());
 
 // 🏡 Task 4: Arguments and Parameters
 /* Substitute the variables in your functions for parameters such that you can substitute `P`, `I`, and `N` when you call the function.
